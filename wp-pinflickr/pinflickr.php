4de272d45a804b8281004003ae37e42fbca55858
brbcoding <cghensha@gmail.com>
2013-09-04 09:41:16 -0400

    added comments and @params details

--- wp-pinflickr/pinflickr.php
+++ wp-pinflickr/pinflickr.php
@@ -8,8 +8,13 @@ Author: Cody G. Henshaw
 Author URI: http://codyhenshaw.com
 License: WTFPL
 */
+?>
 
-// define('WP_DEBUG', true);
+
+<?
+/***************************************************************************************/
+/* SETTINGS AND ADMINISTRATION PAGE
+****************************************************************************************/
 class PinflickrSettingsPage
 {
     /**
@@ -70,24 +75,24 @@ class PinflickrSettingsPage
     public function page_init()
     {        
         register_setting(
-            'pinflickr_options_group', // Option group
-            'pinflickr_options_name', // Option name
-            array( $this, 'sanitize' ) // Sanitize
+            'pinflickr_options_group', 
+            'pinflickr_options_name', 
+            array( $this, 'sanitize' ) 
         );
 
         add_settings_section(
-            'setting_section_id', // ID
-            'My Custom Settings', // Title
+            'setting_section_id', 
+            'My Custom Settings', 
             array( $this, 'print_section_info' ), // Callback
-            'pinflickr-admin' // Page
+            'pinflickr-admin' 
         );  
 
         add_settings_field(
-            'api_key', // ID
-            'API Key', // Title
+            'api_key', 
+            'API Key', 
             array( $this, 'api_key_callback' ), // Callback
-            'pinflickr-admin', // Page
-            'setting_section_id' // Section           
+            'pinflickr-admin', 
+            'setting_section_id'          
         );      
 
         add_settings_field(
@@ -148,43 +153,40 @@ if( is_admin() ) {
     $my_settings_page = new PinflickrSettingsPage();
 }
 
-
-function pinflickr_install() {
-
-
-}
+/***************************************************************************************/
+/* DEPENDENCIES
+****************************************************************************************/
 // load the required scripts
 function pinflickr_includes() {
-	// register the scripts first, then we will enqueue them
-	wp_register_script('jquery-1.10.2', plugin_dir_url(__FILE__) . 'js/jquery-1.10.2.min.js');
-	wp_register_script('freetile_js', plugin_dir_url(__FILE__) . 'js/jquery.freetile.js');
-	wp_register_script('fancybox_js', plugin_dir_url(__FILE__) . 'js/fancybox/source/jquery.fancybox.js');
-	wp_register_script('fancybox_pack', plugin_dir_url(__FILE__) . 'js/fancybox/source/jquery.fancybox.pack.js');
-	wp_register_script('pinflickr_js', plugin_dir_url(__FILE__) . 'js/pinflickr.js');
-	wp_enqueue_script('jquery-1.10.2');
-	wp_enqueue_script('freetile_js');
-	wp_enqueue_script('pinflickr_js');
-	wp_enqueue_script('fancybox_js');
-	wp_enqueue_script('fancybox_pack');
+  // register the scripts first, then we will enqueue them
+  wp_register_script('jquery-1.10.2', plugin_dir_url(__FILE__) . 'js/jquery-1.10.2.min.js');
+  wp_register_script('freetile_js', plugin_dir_url(__FILE__) . 'js/jquery.freetile.js');
+  wp_register_script('fancybox_js', plugin_dir_url(__FILE__) . 'js/fancybox/source/jquery.fancybox.js');
+  wp_register_script('fancybox_pack', plugin_dir_url(__FILE__) . 'js/fancybox/source/jquery.fancybox.pack.js');
+  wp_register_script('pinflickr_js', plugin_dir_url(__FILE__) . 'js/pinflickr.js');
+  wp_enqueue_script('jquery-1.10.2');
+  wp_enqueue_script('freetile_js');
+  wp_enqueue_script('fancybox_js');
+  wp_enqueue_script('fancybox_pack');
+  wp_enqueue_script('pinflickr_js');
 
-	wp_register_style('pinflickr_styles', plugin_dir_url(__FILE__) . 'css/styles.css');
-	wp_enqueue_style('pinflickr_styles');
+  wp_register_style('pinflickr_styles', plugin_dir_url(__FILE__) . 'css/styles.css');
+  wp_enqueue_style('pinflickr_styles');
 
 
-	wp_register_style('fancybox_styles', plugin_dir_url(__FILE__) . 'js/fancybox/source/jquery.fancybox.css');
-	wp_enqueue_style('fancybox_styles');
+  wp_register_style('fancybox_styles', plugin_dir_url(__FILE__) . 'js/fancybox/source/jquery.fancybox.css');
+  wp_enqueue_style('fancybox_styles');
 }
-
+// add the scripts to an action
 add_action('wp_enqueue_scripts', 'pinflickr_includes');
-
-
-// hooks
-add_action('init', 'pinflickr_init');
 /***************************************************************************************/
 /* SHORTCODES
 ****************************************************************************************/
-
-// pass tags and user as the parms of the shortcode
+/**
+ * Create shortcodes for adding the gallery to a page
+ *
+ * @param array $attrs contains array of flickr username and tags to be used
+ */
 function pinflickr_shortcode( $attrs ) {
 
   // get the options from our options page
@@ -209,82 +211,94 @@ add_shortcode('pinflickr', 'pinflickr_shortcode');
 ****************************************************************************************/
 function pinflickr_init() {
   // do stuff
-  // get my secret and API key from the database
-//	$API_KEY = "ffdc6e7cef69d201a7c79bc80477a0ec"; // change this when in prod
-//	$SECRET	 = "a48a5c5114b7ec99"; // change this in prod too
-  //	getFlickrData($SECRET, $API_KEY);
-  	// print_r('init function called');
 }
-
+add_action('init', 'pinflickr_init');
+// if we need to do anything special on the install, do it here
+function pinflickr_install() {
+  // do stuff
+}
+/** 
+ * Handles the request and response to and from flickr
+ *
+ * @param string $SECRET contains string of app secret (from admin page)
+ * @param string $API_KEY contains string of api key (from admin page)
+ * @param string $user_id contains string of user id (from shortcode)
+ * @param string $tags contains string of comma separated tags (from shortcode)
+ */
 function getFlickrData($SECRET, $API_KEY, $user_id, $tags) {
-	// built the request url
-	$url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" . $API_KEY . "&user_id=" . $user_id;
-	
-	// tags should be passed as a comma separated list
-	if($tags != ""){
-		$url .= "&tags=" . $tags;
-	}
-	// format it as json
-	$url .= "&format=json";
-	$curl = curl_init();
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_URL, $url);
-	$res = curl_exec($curl);
-	curl_close($curl);
-	// need to strip this invalid json callback crap
-	$dat = str_replace( 'jsonFlickrApi(', '', $res );
-	$dat = substr( $dat, 0, strlen( $dat ) - 1 ); //strip out last paren
-	$dat = json_decode($dat, TRUE);
-	// return the decoded json response
-	return getFlickrUrls($dat);
+  // build the request url
+  $url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" . $API_KEY . "&user_id=" . $user_id;
+  
+  // tags should be passed as a comma separated list
+  if($tags != ""){
+    $url .= "&tags=" . $tags;
+  }
+  // format it as json
+  $url .= "&format=json";
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_URL, $url);
+  $res = curl_exec($curl);
+  curl_close($curl);
+  // need to strip this invalid json callback crap
+  $dat = str_replace( 'jsonFlickrApi(', '', $res );
+  $dat = substr( $dat, 0, strlen( $dat ) - 1 ); //strip out last paren
+  $dat = json_decode($dat, TRUE);
+  // return the decoded json response
+  return getFlickrUrls($dat);
 }
 
-// title is stored in $pic['title']
+/** 
+ * Builds the list of urls and associated titles
+ *
+ * @param array $dat contains the urls and titles from json response in getFlickrData()
+ */
 function getFlickrUrls($dat){
-	$urls = array();
-	$nums = 0;
-	if($dat){
-		foreach($dat['photos']['photo'] as $pic){  
-			// build the url
-			$photo_url	  = 'http://farm' . $pic['farm'] . '.staticflickr.com/' . $pic['server'] . 
-							'/' . $pic['id'] . '_' . $pic['secret'] . ".jpg";
-			// create a temporary array with the title and the photo's url
-            // it should contain the title and the photo's url
-			$urls[$nums]['title'] = $pic['title'];		
-			$urls[$nums]['url'] = $photo_url;
-			$nums++;
-		}
-	} else {
-		echo "Flickr Was Unreachable.";
-	}
-	return buildPinflickrHtml($urls);
+  $urls = array();
+  $nums = 0;
+  if($dat){
+    foreach($dat['photos']['photo'] as $pic){  
+      $photo_url    = 'http://farm' . $pic['farm'] . '.staticflickr.com/' . $pic['server'] . 
+              '/' . $pic['id'] . '_' . $pic['secret'] . ".jpg";
+      // create a temporary array with the title and the photo's url
+      $urls[$nums]['title'] = $pic['title'];    
+      $urls[$nums]['url'] = $photo_url;
+      $nums++;
+    }
+  } else {
+    echo "Flickr Was Unreachable.";
+  }
+  return buildPinflickrHtml($urls);
 }
 
-
-// build an html string to output to the page
-// it should contain the title of the image as
-// well as the url in image form
+/** 
+ * Builds the html to be displayed and returns it as a string
+ */
 function buildPinflickrHtml($urls) {
-	$html = "<div id='container'>";
-	if($urls){
-		foreach($urls as $url) {
-			// t_html for temporary string builder
-			// set to "" after each iteration
-			$t_html = "";
-			$t_html .= '<div class="item subtle rotate"><a class="fancybox" href="' . 
-				$url['url']   .'"><img class="pin" src="' .
-				$url['url']   . '" alt="' .
-				$url['title'] .'" title="' .
-				$url['title'] .'"></a><br /><span class="image-title">' .
-				$url['title'] . '...</span></div>';
-			$html .= $t_html;
-		}
+  $html = "<div id='container'>";
+  if($urls){
+    foreach($urls as $url) {
+      // t_html for temporary string builder
+      // set to "" after each iteration
+      $t_html = "";
+      $t_html .= '<div class="item subtle rotate"><a class="fancybox" href="' . 
+        $url['url']   .'"><img class="pin" src="' .
+        $url['url']   . '" alt="' .
+        $url['title'] .'" title="' .
+        $url['title'] .'"></a><br /><span class="image-title">' .
+        $url['title'] . '...</span></div>';
+      $html .= $t_html;
+    }
 
-		// close the #container
-		$html .= "</div>";
-		return $html;		
-	}
+    // close the #container
+    $html .= "</div>";
+    return $html;   
+  }
 
 }
+/** 
+ * Debug
+ */
+// define('WP_DEBUG', true);
 
 ?>
